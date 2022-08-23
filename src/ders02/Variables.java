dddb61479c9bc70f7826e436b4359f8608d47c38
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-08-23 15:39:29 +0200

    2.ders

---
+++ src/ders02/Variables.java
@@ -0,0 +1,23 @@
+package ders02;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int sayi; // deklarasyon sadece 1 kere yapilir
+
+        sayi=15; // deger atamasi istedigimiz kadar olabilir
+        System.out.println(sayi); // 15
+
+        sayi=20;
+        System.out.println(sayi); // 20
+
+        sayi = sayi+10;
+
+        //      20+10 =>30
+        // sayi=30;
+
+        System.out.println(sayi); // 30
+
+
+    }
+}
